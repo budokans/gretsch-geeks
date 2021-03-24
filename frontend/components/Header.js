fbb71b1360edc428dd3c4db9100c4ee526f233b5
budokans <68995729+budokans@users.noreply.github.com>
2021-03-24 20:59:07 +1300

    Created Logo and Container
    Styled components within Header

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,18 +1,51 @@
 import Link from 'next/link';
+import styled from 'styled-components';
 import Nav from './Nav';
+
+const Logo = styled.h1`
+  font-size: 4em;
+  background: red;
+  margin-left: 2rem;
+  transform: skew(-7deg);
+  z-index: 2;
+  position: relative;
+
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
+
+const Container = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .sub-bar {
+    border-bottom: 1px solid var(--black, black);
+    display: grid;
+    grid-template-columns: 1fr auto;
+  }
+`;
 
 export default function Header() {
   return (
-    <header>
+    <Container>
       <div className="bar">
-        <Link a href="/">
-          Sick Fits
-        </Link>
+        <Logo>
+          <Link href="/">Sick Fits</Link>
+        </Logo>
       </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
       <Nav />
-    </header>
+    </Container>
   );
 }
