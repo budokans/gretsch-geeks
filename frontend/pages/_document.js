a46e3f3fa9db536393c19789add0cf3627dc7bb2
budokans <68995729+budokans@users.noreply.github.com>
2021-03-24 22:09:11 +1300

    Fixed FOUC
    by injecting SRR styles into <head> for initial render.

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,6 +1,17 @@
 import Document, { Html, Head, NextScript, Main } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  // Inject server side rendered styles into <head> for first render (and then load the rest in the client)
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <Html lang="en-NZ">
