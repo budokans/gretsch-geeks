fff6f7c11cb821555fe58aaa8b1061b4378ddc54
budokans <68995729+budokans@users.noreply.github.com>
2021-03-25 01:44:27 +1300

    Added withAuth to Keystone

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,33 +1,60 @@
-import 'dotenv/config';
+import { createAuth } from '@keystone-next/auth';
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
+import 'dotenv/config';
 
+// What database is Keystone connecting to
 const databaseURL = process.env.DATABASE_URL;
 
+// What session configuration is each connected governed by
 const sessionConfig = {
   maxAge: 60 * 60 * 24 * 360,
   secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
+const { withAuth } = createAuth({
+  // Which schema is responsible for user authentication
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  // In case no user exists yet
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TO DO: add initial roles here
   },
-  db: {
-    adapter: 'mongoose',
-    url: databaseURL,
-    // TO DO: add seed data
-  },
-  lists: createSchema({
-    // TO DO: add schema items
-    User,
-  }),
-  ui: {
-    // TO DO: change this for roles
-    isAccessAllowed: () => true,
-  },
-  // TO DO: add session values here
 });
+
+// What frontend server is connecting to the Keystone backend, which db typ etc
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      // TO DO: add seed data
+    },
+    lists: createSchema({
+      // TO DO: add schema items
+      User,
+    }),
+    ui: {
+      // Show UI to only those who pass this test
+      isAccessAllowed: ({ session }) => {
+        console.log(session);
+        return !!session?.data;
+      },
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id',
+    }),
+  })
+);
