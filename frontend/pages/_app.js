6223e2ff5b6f7894301f8f36d5b68b35fe35e7f8
budokans <68995729+budokans@users.noreply.github.com>
2021-03-25 20:58:32 +1300

    Hooked up Next.js frontend with Apollo

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,8 @@
 import NProgress from 'nprogress';
 import Router from 'next/router';
 import PropTypes from 'prop-types';
+import { ApolloProvider } from '@apollo/client';
+import withApollo from '../lib/withData';
 import Page from '../components/Page';
 import '../components/styles/nprogress.css';
 
@@ -9,15 +11,29 @@ Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-export default function App({ Component, pageProps }) {
+function App({ Component, pageProps, apollo }) {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
 
 App.propTypes = {
   Component: PropTypes.func,
   pageProps: PropTypes.any,
+  apollo: PropTypes.object,
 };
+
+App.getInitialProps = async ({ Component, ctx }) => {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withApollo(App);
