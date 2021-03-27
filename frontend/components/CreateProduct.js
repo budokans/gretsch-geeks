a1d7223e26507cf0990c357cba8351efecb7282a
budokans <68995729+budokans@users.noreply.github.com>
2021-03-27 14:35:46 +1300

    Refetching all products on submit new product
    Prevents only previously cached products data being available without refresh.

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -3,6 +3,7 @@ import gql from 'graphql-tag';
 import useForm from '../lib/useForm';
 import Form from './styles/Form';
 import ErrorMessage from './ErrorMessage';
+import { ALL_PRODUCTS_QUERY } from './Products';
 
 const CREATE_PRODUCT_MUTATION = gql`
   mutation CREATE_PRODUCT_MUTATION(
@@ -36,10 +37,13 @@ export default function CreateProduct() {
     price: '',
   });
 
-  const [
-    createProduct,
-    { data, error, loading },
-  ] = useMutation(CREATE_PRODUCT_MUTATION, { variables: inputs });
+  const [createProduct, { data, error, loading }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+      refetchQueries: [{ query: ALL_PRODUCTS_QUERY }],
+    }
+  );
 
   return (
     <Form
