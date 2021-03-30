e06d4f1be4507b5d139c1574937804850041ccd4
budokans <68995729+budokans@users.noreply.github.com>
2021-03-30 23:48:38 +1300

    Create update form and initialise state

--- frontend/components/UpdateProduct.js
+++ frontend/components/UpdateProduct.js
@@ -1,6 +1,9 @@
 import PropTypes from 'prop-types';
 import gql from 'graphql-tag';
 import { useMutation, useQuery } from '@apollo/client';
+import useForm from '../lib/useForm';
+import DisplayError from './ErrorMessage';
+import Form from './styles/Form';
 
 const SINGLE_PRODUCT_QUERY = gql`
   query SINGLE_PRODUCT_QUERY($id: ID!) {
@@ -41,7 +44,59 @@ export default function UpdateProduct({ id }) {
     { data: updateData, error: updateError, loading: updateLoading },
   ] = useMutation(UPDATE_PRODUCT_MUTATION, { variables: { id } });
 
-  return <p>Update {id}</p>;
+  const { inputs, handleChange, clearForm, resetForm } = useForm(data?.Product);
+
+  return (
+    <Form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        // TO DO: handle submit
+        // const res = await createProduct();
+        // clearForm();
+        // Router.push(`/products/${res.data.createProduct.id}`);
+      }}
+    >
+      <DisplayError error={error || updateError} />
+      <fieldset disabled={updateLoading} aria-busy={updateLoading}>
+        <label htmlFor="name">
+          Name
+          <input
+            required
+            type="text"
+            id="name"
+            name="name"
+            placeholder="name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            required
+            type="number"
+            id="price"
+            name="price"
+            placeholder="Price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button type="submit">+ Add Product</button>
+      </fieldset>
+    </Form>
+  );
 }
 
 UpdateProduct.propTypes = {
