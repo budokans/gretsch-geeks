0ffde9e4043c8afd2fe4c0a0ebeb514d9307d620
budokans <68995729+budokans@users.noreply.github.com>
2021-03-30 23:44:43 +1300

    Single product query created and accessed

--- frontend/components/UpdateProduct.js
+++ frontend/components/UpdateProduct.js
@@ -1,6 +1,22 @@
 import PropTypes from 'prop-types';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/client';
+
+const SINGLE_PRODUCT_QUERY = gql`
+  query SINGLE_PRODUCT_QUERY($id: ID!) {
+    Product(where: { id: $id }) {
+      name
+      description
+      price
+    }
+  }
+`;
 
 export default function UpdateProduct({ id }) {
+  const { data, error, loading } = useQuery(SINGLE_PRODUCT_QUERY, {
+    variables: { id },
+  });
+
   return <p>Update {id}</p>;
 }
 
