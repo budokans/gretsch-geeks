b4d149fd9aa36aceec915269febaeedcdacacd14
budokans <68995729+budokans@users.noreply.github.com>
2021-03-30 23:45:32 +1300

    Update product mutation created and accessed

--- frontend/components/UpdateProduct.js
+++ frontend/components/UpdateProduct.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import gql from 'graphql-tag';
-import { useQuery } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
 
 const SINGLE_PRODUCT_QUERY = gql`
   query SINGLE_PRODUCT_QUERY($id: ID!) {
@@ -12,10 +12,34 @@ const SINGLE_PRODUCT_QUERY = gql`
   }
 `;
 
+const UPDATE_PRODUCT_MUTATION = gql`
+  mutation UPDATE_PRODUCT_MUTATION(
+    $id: ID!
+    $name: String
+    $description: String
+    $price: Int
+  ) {
+    updateProduct(
+      id: $id
+      data: { id: $id, name: $name, description: $description, price: $price }
+    ) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+
 export default function UpdateProduct({ id }) {
   const { data, error, loading } = useQuery(SINGLE_PRODUCT_QUERY, {
     variables: { id },
   });
+
+  const [
+    updateProduct,
+    { data: updateData, error: updateError, loading: updateLoading },
+  ] = useMutation(UPDATE_PRODUCT_MUTATION, { variables: { id } });
 
   return <p>Update {id}</p>;
 }
