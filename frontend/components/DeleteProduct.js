6cec0c53d124ad0610094f5575eed5adfa6d476b
budokans <68995729+budokans@users.noreply.github.com>
2021-03-31 18:56:49 +1300

    Successfully deleting product on click

--- frontend/components/DeleteProduct.js
+++ frontend/components/DeleteProduct.js
@@ -1,14 +1,34 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
 import PropTypes from 'prop-types';
 
+const DELETE_PRODUCT_MUTATION = gql`
+  mutation DELETE_PRODUCT_MUTATION($id: ID!) {
+    deleteProduct(id: $id) {
+      id
+      name
+    }
+  }
+`;
+
 export default function DeleteProduct({ id, children }) {
+  const [deleteProduct, { loading }] = useMutation(DELETE_PRODUCT_MUTATION, {
+    variables: { id },
+  });
+
   return (
     <button
       type="button"
-      onClick={() =>
-        confirm(
-          "Are you sure you want to delete this product? There's no going back!"
-        )
-      }
+      disabled={loading}
+      onClick={() => {
+        if (
+          confirm(
+            "Are you sure you want to delete this product? There's no going back!"
+          )
+        ) {
+          deleteProduct().catch((err) => alert(err.message));
+        }
+      }}
     >
       {children}
     </button>
