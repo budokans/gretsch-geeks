98e62d451c9c29914a8dec6c43be0d9f21861c65
budokans <68995729+budokans@users.noreply.github.com>
2021-03-31 18:29:41 +1300

    Added onClick with confirm() to button

--- frontend/components/DeleteProduct.js
+++ frontend/components/DeleteProduct.js
@@ -1,7 +1,18 @@
 import PropTypes from 'prop-types';
 
 export default function DeleteProduct({ id, children }) {
-  return <button type="button">{children}</button>;
+  return (
+    <button
+      type="button"
+      onClick={() =>
+        confirm(
+          "Are you sure you want to delete this product? There's no going back!"
+        )
+      }
+    >
+      {children}
+    </button>
+  );
 }
 
 DeleteProduct.propTypes = {
