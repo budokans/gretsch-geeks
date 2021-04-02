9b9c87d65440a2e38b5f1c26e7db78eb92ec0be3
budokans <68995729+budokans@users.noreply.github.com>
2021-04-02 19:03:25 +1300

    Remove unnecessary console.logs

--- frontend/lib/paginationField.js
+++ frontend/lib/paginationField.js
@@ -4,7 +4,6 @@ export default function paginationField() {
   return {
     keyArgs: false,
     read(existing = [], { args, cache }) {
-      console.log({ existing, args, cache });
       const { skip, first } = args;
 
       // Read the number of items on page from cache
@@ -24,22 +23,18 @@ export default function paginationField() {
       if (items.length !== first) return false;
 
       // If there are items, return them from the cache
-      if (items.length) {
-        console.log(`There are ${items.length} items in the cache.`);
-        return items;
-      }
+      if (items.length) return items;
 
       return false;
     },
     merge(existing, incoming, { args }) {
       const { skip } = args;
-      console.log(`Merging ${incoming.length} from the network.`);
       const merged = existing ? existing.slice(0) : [];
 
       for (let i = skip; i < skip + incoming.length; ++i) {
         merged[i] = incoming[i - skip];
       }
-      console.log(merged);
+
       return merged;
     },
   };
