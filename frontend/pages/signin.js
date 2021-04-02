8525864351026b3ab3b634a4340fe9bd0159aae9
budokans <68995729+budokans@users.noreply.github.com>
2021-04-02 23:35:34 +1300

    Styled Grid container

--- frontend/pages/signin.js
+++ frontend/pages/signin.js
@@ -1,11 +1,25 @@
+import styled from 'styled-components';
 import SignIn from '../components/SignIn';
 import RegisterUser from '../components/RegisterUser';
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 3rem;
+  max-width: 1000px;
+  margin: 0 auto;
+
+  @media (max-width: 600px) {
+    grid-template-columns: 1fr;
+    grid-gap: 2rem;
+  }
+`;
+
 export default function SignInPage() {
   return (
-    <>
+    <Grid>
       <SignIn />
       <RegisterUser />
-    </>
+    </Grid>
   );
 }
