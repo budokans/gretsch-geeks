caef1df21666d9407642bb5f1ded871684f3ad26
budokans <68995729+budokans@users.noreply.github.com>
2021-04-02 19:01:59 +1300

    Create current user query and hook

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,7 +1,10 @@
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
+import { useUser } from './User';
 
 export default function Nav() {
+  const user = useUser();
+  console.log(user);
   return (
     <NavStyles>
       <Link href="/products">Products</Link>
