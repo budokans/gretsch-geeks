d3b2a4dae3edf81adb6eee160a3e724b988d2f88
budokans <68995729+budokans@users.noreply.github.com>
2021-04-03 23:44:36 +1300

    Create email template function

--- backend/lib/mail.ts
+++ backend/lib/mail.ts
@@ -8,3 +8,19 @@ const transporter = createTransport({
     pass: MAIL_PASS,
   },
 });
+
+function makeNiceEmail(text: string) {
+  return `
+    <div style="
+      border: 1px solid black; 
+      padding: 20px; 
+      font-family: sans-serif; 
+      line-height: 1.6; 
+      font-size: 20px;
+    ">
+      <h2>Hello!</h2>
+      <p>${text}</p>
+      <p>Cheers, Steven Webster</p>
+    </div>
+  `;
+}
