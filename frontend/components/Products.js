e08a15e640fb352385ce3b63b1ddb6ee24a86479
budokans <68995729+budokans@users.noreply.github.com>
2021-04-07 02:16:10 +1200

    Bug fix
    Reroute to products/pageCount if user deletes all products from last page and there is therefore no data to map over and display on page.

--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -2,8 +2,10 @@ import { useQuery } from '@apollo/client';
 import PropTypes from 'prop-types';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
+import Router from 'next/router';
 import Product from './Product';
 import { perPage } from '../config';
+import { PAGINATION_QUERY } from './Pagination';
 
 export const ALL_PRODUCTS_QUERY = gql`
   query ALL_PRODUCTS_QUERY($skip: Int = 0, $first: Int) {
@@ -31,12 +33,21 @@ export default function Products({ pageNum }) {
   const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY, {
     variables: { skip: pageNum * perPage - perPage, first: perPage },
   });
+
+  const { data: countData } = useQuery(PAGINATION_QUERY);
+  const productsCount = countData?._allProductsMeta.count;
+  const pageCount = Math.ceil(productsCount / perPage);
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Oh no! Error: {error.message}</p>;
 
+  if (pageNum > pageCount) {
+    Router.push(`/products/${pageCount}`);
+  }
+
   return (
     <ProductsListStyles>
-      {data.allProducts.map((product) => (
+      {data?.allProducts.map((product) => (
         <Product key={product.id} product={product} />
       ))}
     </ProductsListStyles>
