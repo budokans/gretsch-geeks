799945d3d233d16003c5a64edf3af143230d9788
budokans <68995729+budokans@users.noreply.github.com>
2021-04-06 21:27:53 +1200

    Create AddToCart component

---
+++ frontend/components/AddToCart.js
@@ -0,0 +1,18 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+
+const ADD_TO_CART_MUTATION = gql`
+  mutation ADD_TO_CART_MUTATION($id: ID!) {
+    addToCart(productId: $id) {
+      id
+    }
+  }
+`;
+
+export default function AddToCart({ id }) {
+  const [addToCart, { loading }] = useMutation(ADD_TO_CART_MUTATION, {
+    variables: { id },
+  });
+
+  return <button type="button">Add to Cart 🛒</button>;
+}
