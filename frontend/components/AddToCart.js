1cec0a75549a4c2f8fd7c50f59606f8f2120154b
budokans <68995729+budokans@users.noreply.github.com>
2021-04-06 21:39:00 +1200

    refetch User query on addToCart
    to rerender cart with updated items and totals

--- frontend/components/AddToCart.js
+++ frontend/components/AddToCart.js
@@ -1,6 +1,7 @@
 import { useMutation } from '@apollo/client';
 import PropTypes from 'prop-types';
 import gql from 'graphql-tag';
+import { CURRENT_USER_QUERY } from './User';
 
 const ADD_TO_CART_MUTATION = gql`
   mutation ADD_TO_CART_MUTATION($id: ID!) {
@@ -13,6 +14,7 @@ const ADD_TO_CART_MUTATION = gql`
 export default function AddToCart({ id }) {
   const [addToCart, { loading }] = useMutation(ADD_TO_CART_MUTATION, {
     variables: { id },
+    refetchQueries: [{ query: CURRENT_USER_QUERY }],
   });
 
   return (
