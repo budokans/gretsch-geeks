eb54384b7eaace14d462dd9647c01da469cb0460
budokans <68995729+budokans@users.noreply.github.com>
2021-04-06 23:28:05 +1200

    Animate Dot on addToCart

--- frontend/components/CartCount.js
+++ frontend/components/CartCount.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const Dot = styled.div`
   background: var(--red);
@@ -12,8 +13,46 @@ const Dot = styled.div`
   font-variant-numeric: tabular-nums;
 `;
 
+const AnimationStyles = styled.span`
+  position: relative;
+
+  .count {
+    display: block;
+    position: relative;
+    transition: transform 0.4s;
+    backface-visibility: hidden;
+  }
+
+  .count-enter {
+    transform: scale(4) rotateY(0.5turn);
+  }
+
+  .count-enter-active {
+    transform: rotateX(0);
+  }
+
+  .count-exit {
+    top: 0;
+    position: absolute;
+  }
+`;
+
 export default function CartCount({ count }) {
-  return <Dot>{count}</Dot>;
+  return (
+    <AnimationStyles>
+      <TransitionGroup>
+        <CSSTransition
+          unmountOnExit
+          className="count"
+          classNames="count"
+          key={count}
+          timeout={{ enter: 400, exit: 400 }}
+        >
+          <Dot>{count}</Dot>
+        </CSSTransition>
+      </TransitionGroup>
+    </AnimationStyles>
+  );
 }
 
 CartCount.propTypes = {
