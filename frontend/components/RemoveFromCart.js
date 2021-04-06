6977df6f6ef00da00bc7df474dceb8f1ecbdd164
budokans <68995729+budokans@users.noreply.github.com>
2021-04-07 01:52:57 +1200

    Evict deleted cart item from cache

--- frontend/components/RemoveFromCart.js
+++ frontend/components/RemoveFromCart.js
@@ -11,9 +11,14 @@ const REMOVE_FROM_CART_MUTATION = gql`
   }
 `;
 
+function update(cache, payload) {
+  cache.evict(cache.identify(payload.data.deleteCartItem));
+}
+
 export default function RemoveFromCart({ id }) {
   const [deleteCartItem, { loading }] = useMutation(REMOVE_FROM_CART_MUTATION, {
     variables: { id },
+    update,
   });
 
   return (
