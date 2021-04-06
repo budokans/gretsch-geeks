5f27c47503afa06c3d45bed8983313e365c4a66b
budokans <68995729+budokans@users.noreply.github.com>
2021-04-07 03:16:33 +1200

    Integrate downshift

--- frontend/components/Search.js
+++ frontend/components/Search.js
@@ -1,10 +1,31 @@
+import { resetIdCounter, useCombobox } from 'downshift';
 import { DropDown, DropDownItem, SearchStyles } from './styles/DropDown';
 
 export default function Search() {
+  resetIdCounter();
+  const { getMenuProps, getInputProps, getComboboxProps } = useCombobox({
+    items: [],
+    onInputValueChange() {
+      console.log('Input value changed.');
+    },
+    onSelectedItemChange() {
+      console.log('Selected item changed.');
+    },
+  });
+
   return (
     <SearchStyles>
-      <input type="search" />
-      <DropDown>
+      <div {...getComboboxProps()}>
+        <input
+          {...getInputProps({
+            type: 'search',
+            placeholder: 'Search for items',
+            id: 'search',
+            className: 'loading',
+          })}
+        />
+      </div>
+      <DropDown {...getMenuProps()}>
         <DropDownItem>1</DropDownItem>
         <DropDownItem>2</DropDownItem>
         <DropDownItem>3</DropDownItem>
