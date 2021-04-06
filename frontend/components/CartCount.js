f86f2fe2eabe0d88efea34ec6e5d892e3bcfdc2a
budokans <68995729+budokans@users.noreply.github.com>
2021-04-06 22:09:34 +1200

    Style Dot component

--- frontend/components/CartCount.js
+++ frontend/components/CartCount.js
@@ -1,7 +1,16 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const Dot = styled.div``;
+const Dot = styled.div`
+  background: var(--red);
+  border-radius: 50%;
+  padding: 0.5rem;
+  color: white;
+  margin-left: 0.5rem;
+  /* Make digits same width */
+  font-feature-settings: 'tnum';
+  font-variant-numeric: tabular-nums;
+`;
 
 export default function CartCount({ count }) {
   return <Dot>{count}</Dot>;
