b2f53e7514df65d00bf8db965a1cc796a1eb4e20
budokans <68995729+budokans@users.noreply.github.com>
2021-04-08 01:47:04 +1200

    Form now in separate component
    So that the entire component is wrapped in Stripe's Elements provider element - useStripe() can't be called elsewhere.

--- frontend/components/Checkout.js
+++ frontend/components/Checkout.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { loadStripe } from '@stripe/stripe-js';
-import { CardElement, Elements } from '@stripe/react-stripe-js';
+import { CardElement, Elements, useStripe } from '@stripe/react-stripe-js';
+import { useState } from 'react';
 import SickButton from './styles/SickButton';
 
 const CheckoutFormStyles = styled.form`
@@ -14,18 +15,35 @@ const CheckoutFormStyles = styled.form`
 
 const stripeLib = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY);
 
-export default function Checkout() {
+function CheckoutForm() {
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+  const stripe = useStripe();
+
   function handleSubmit(e) {
+    // 1. Stop the form from submitting and turn the loader on
     e.preventDefault();
-    console.log('Checking out now.');
+    // 2. Start the page transition
+    // 3. Create the payment method via Stripe (token comes back here if success)
+    // 4. Handle any Stripe errors
+    // 5. Send token to Keystone server via custom mutation
+    // 6. Route to the order page
+    // 7. Close cart
+    // 8. Turn off loader
   }
 
   return (
+    <CheckoutFormStyles onSubmit={handleSubmit}>
+      <CardElement />
+      <SickButton type="submit">Pay Now</SickButton>
+    </CheckoutFormStyles>
+  );
+}
+
+export default function Checkout() {
+  return (
     <Elements stripe={stripeLib}>
-      <CheckoutFormStyles onSubmit={handleSubmit}>
-        <CardElement />
-        <SickButton type="submit">Pay Now</SickButton>
-      </CheckoutFormStyles>
+      <CheckoutForm />
     </Elements>
   );
 }
