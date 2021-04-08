c2d7961587503feb4f65ccc2e8fd2a59cc657ab8
budokans <68995729+budokans@users.noreply.github.com>
2021-04-09 00:33:39 +1200

    Create USER_ORDERS_QUERY and display data

--- frontend/pages/orders.js
+++ frontend/pages/orders.js
@@ -1,3 +1,48 @@
-export default function OrderPage() {
-  return <h2>Order page</h2>;
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import Head from 'next/head';
+import formatMoney from '../lib/formatMoney';
+import DisplayError from '../components/ErrorMessage';
+import OrderStyles from '../components/styles/OrderStyles';
+
+const USER_ORDERS_QUERY = gql`
+  query USER_ORDERS_QUERY {
+    allOrders {
+      id
+      charge
+      total
+      user {
+        id
+        name
+      }
+      items {
+        id
+        name
+        description
+        price
+        quantity
+        photo {
+          image {
+            publicUrlTransformed
+          }
+          altText
+        }
+      }
+    }
+  }
+`;
+
+export default function OrdersPage() {
+  const { data, error, loading } = useQuery(USER_ORDERS_QUERY);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <DisplayError error={error} />;
+
+  const { allOrders } = data;
+
+  return (
+    <div>
+      <h2>You have {allOrders.length} orders!</h2>
+    </div>
+  );
 }
