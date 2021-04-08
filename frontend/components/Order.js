9b56968d123e10c2dfdc8de8d3e862269b4e42b0
budokans <68995729+budokans@users.noreply.github.com>
2021-04-09 00:19:29 +1200

    Display data, move logic to Order component

--- frontend/components/Order.js
+++ frontend/components/Order.js
@@ -1,7 +1,85 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
 import PropTypes from 'prop-types';
+import Head from 'next/head';
+import formatMoney from '../lib/formatMoney';
+import DisplayError from './ErrorMessage';
+import OrderStyles from './styles/OrderStyles';
+
+const SINGLE_ORDER_QUERY = gql`
+  query SINGLE_ORDER_QUERY($id: ID!) {
+    order: Order(where: { id: $id }) {
+      id
+      charge
+      total
+      user {
+        id
+        name
+      }
+      items {
+        id
+        name
+        description
+        price
+        quantity
+        photo {
+          image {
+            publicUrlTransformed
+          }
+          altText
+        }
+      }
+    }
+  }
+`;
 
 export default function Order({ id }) {
-  return <p>This is the page for order {id}</p>;
+  const { data, error, loading } = useQuery(SINGLE_ORDER_QUERY, {
+    variables: { id },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <DisplayError error={error} />;
+
+  const { order } = data;
+
+  return (
+    <OrderStyles>
+      <Head>
+        <title>Sick Fits | {order.user.name}'s Order</title>
+      </Head>
+      <p>
+        <span>Order ID:</span>
+        <span>{order.id}</span>
+      </p>
+      <p>
+        <span>Item count:</span>
+        <span>{order.items.length}</span>
+      </p>
+      <p>
+        <span>Order total:</span>
+        <span>{formatMoney(order.total)}</span>
+      </p>
+
+      <div className="items">
+        {order.items.map((item) => (
+          <div className="order-item" key={item.id}>
+            <img
+              src={item.photo.image.publicUrlTransformed}
+              alt={item.photo.altText}
+            />
+            <div className="item-details">
+              <h2>{item.name}</h2>
+              <p>Qty: {item.quantity}</p>
+              <p>Each: {formatMoney(item.price)}</p>
+              <p>Subtotal: {formatMoney(item.price * item.quantity)}</p>
+              <p>{item.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </OrderStyles>
+  );
 }
 
 Order.propTypes = {
