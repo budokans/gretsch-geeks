06f4ce838dd919e283b80028a8cddaa2c40ea8fc
budokans <68995729+budokans@users.noreply.github.com>
2021-04-09 00:00:10 +1200

    Query order and render data on page.

--- frontend/pages/order/[id].js
+++ frontend/pages/order/[id].js
@@ -1,8 +1,47 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
 import PropTypes from 'prop-types';
 import Order from '../../components/Order';
+import DisplayError from '../../components/ErrorMessage';
+
+const SINGLE_ORDER_QUERY = gql`
+  query SINGLE_ORDER_QUERY($id: ID!) {
+    order: Order(where: { id: $id }) {
+      id
+      charge
+      total
+      user {
+        id
+      }
+      items {
+        id
+        name
+        description
+        price
+        quantity
+        photo {
+          image {
+            publicUrlTransformed
+          }
+          altText
+        }
+      }
+    }
+  }
+`;
 
 export default function SingleOrderPage({ query }) {
-  return <Order id={query.id} />;
+  const { data, error, loading } = useQuery(SINGLE_ORDER_QUERY, {
+    variables: { id: query.id },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <DisplayError error={error} />;
+
+  const { order } = data;
+
+  // return <Order id={order.id} />;
+  return <p>This is the page for order: {order.id}</p>;
 }
 
 SingleOrderPage.propTypes = {
