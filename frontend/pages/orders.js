3dda5d91f9f63b1f13adedb5e1d468965e9d58b8
budokans <68995729+budokans@users.noreply.github.com>
2021-04-09 00:52:54 +1200

    Style elements and display orders data

--- frontend/pages/orders.js
+++ frontend/pages/orders.js
@@ -1,9 +1,11 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 import Head from 'next/head';
+import Link from 'next/link';
+import styled from 'styled-components';
 import formatMoney from '../lib/formatMoney';
 import DisplayError from '../components/ErrorMessage';
-import OrderStyles from '../components/styles/OrderStyles';
+import OrderItemStyles from '../components/styles/OrderItemStyles';
 
 const USER_ORDERS_QUERY = gql`
   query USER_ORDERS_QUERY {
@@ -32,6 +34,16 @@ const USER_ORDERS_QUERY = gql`
   }
 `;
 
+const OrdersUl = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  grid-gap: 4rem;
+`;
+
+function countOrderItems(order) {
+  return order.items.reduce((tally, item) => tally + item.quantity, 0);
+}
+
 export default function OrdersPage() {
   const { data, error, loading } = useQuery(USER_ORDERS_QUERY);
 
@@ -42,7 +54,39 @@ export default function OrdersPage() {
 
   return (
     <div>
+      <Head>
+        <title>Sick Fits | Your Orders ({allOrders.length})</title>
+      </Head>
       <h2>You have {allOrders.length} orders!</h2>
+      <OrdersUl>
+        {allOrders.map((order) => (
+          <OrderItemStyles key={order.id}>
+            <Link href={`/order/${order.id}`}>
+              <a>
+                <div className="order-meta">
+                  <p>{countOrderItems(order)} items</p>
+                  <p>
+                    {`${order.items.length} product${
+                      order.items.length > 1 ? 's' : ''
+                    }
+                  `}
+                  </p>
+                  <p>{formatMoney(order.total)}</p>
+                </div>
+                <div className="images">
+                  {order.items.map((item) => (
+                    <img
+                      key={item.id}
+                      src={item.photo.image.publicUrlTransformed}
+                      alt={item.photo.altText}
+                    />
+                  ))}
+                </div>
+              </a>
+            </Link>
+          </OrderItemStyles>
+        ))}
+      </OrdersUl>
     </div>
   );
 }
