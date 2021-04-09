bf66f473849d4615ae83aeeda57e7754008d902e
budokans <68995729+budokans@users.noreply.github.com>
2021-04-09 17:45:27 +1200

    Generate and export permissions

--- backend/access.ts
+++ backend/access.ts
@@ -1,5 +1,19 @@
+import { Permission, permissionsList } from './schemas/fields';
 import { ListAccessArgs } from './types';
 
 export function isSignedIn({ session }: ListAccessArgs): boolean {
   return !!session;
 }
+
+const generatedPermissions = Object.fromEntries(
+  permissionsList.map((permission: Permission) => [
+    permission,
+    function ({ session }: ListAccessArgs): boolean {
+      return !!session?.data.role?.[permission];
+    },
+  ])
+);
+
+export const permissions = {
+  ...generatedPermissions,
+};
