e9d3262f23042f02f3ecb0239e0de00bb2a5c5fc
budokans <68995729+budokans@users.noreply.github.com>
2021-04-11 14:36:27 +1200

    Create gated SignIn component and wrap sell page

--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,5 +1,10 @@
 import CreateProduct from '../components/CreateProduct';
+import PleaseSignIn from '../components/PleaseSignIn';
 
 export default function SellPage() {
-  return <CreateProduct />;
+  return (
+    <PleaseSignIn>
+      <CreateProduct />
+    </PleaseSignIn>
+  );
 }
