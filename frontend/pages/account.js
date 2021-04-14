341612316296de30c96937314b1ef70784f16e02
budokans <68995729+budokans@users.noreply.github.com>
2021-04-14 21:34:54 +1200

    Display username and RequestReset

--- frontend/pages/account.js
+++ frontend/pages/account.js
@@ -1,14 +1,19 @@
 import Head from 'next/head';
 import PleaseSignIn from '../components/PleaseSignIn';
+import RequestReset from '../components/RequestReset';
+import { useUser } from '../components/User';
 
 export default function AccountPage() {
+  const me = useUser();
+
   return (
     <>
       <Head>
-        <title>Sick Fits | Your Account</title>
+        <title>Sick Fits | {`${!me ? 'Your' : me.name}'s Account`}</title>
       </Head>
       <PleaseSignIn>
-        <h2>Account page</h2>
+        <h2>Signed in as {me.name}</h2>
+        <RequestReset />
       </PleaseSignIn>
     </>
   );
