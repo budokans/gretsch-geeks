50b9a98be18abda3c88d2e0cad6843c27e992dba
budokans <68995729+budokans@users.noreply.github.com>
2021-04-17 15:56:30 +1200

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = `https://sfserver.stevenwebster.co:35770/api/graphql`;
+export const prodEndpoint = `https://sfserver.stevenwebster.co/api/graphql`;
 export const perPage = 4;
