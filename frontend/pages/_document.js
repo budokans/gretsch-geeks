0856def20f6ba2c59227fa6542c7a0c0c5277642
budokans <68995729+budokans@users.noreply.github.com>
2021-04-28 00:12:48 +1200

    Tidy up _document.js

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -2,16 +2,6 @@ import Document, { Html, Head, NextScript, Main } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  // Inject server side rendered styles into <head> for first render (and then load the rest in the client)
-  // static getInitialProps({ renderPage }) {
-  //   const sheet = new ServerStyleSheet();
-  //   const page = renderPage((App) => (props) =>
-  //     sheet.collectStyles(<App {...props} />)
-  //   );
-  //   const styleTags = sheet.getStyleElement();
-  //   return { ...page, styleTags };
-  // }
-
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
