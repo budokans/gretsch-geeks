92ea0a169ba70d7a9881542c4ada28782a542622
budokans <68995729+budokans@users.noreply.github.com>
2021-04-28 00:06:56 +1200

    Fix FOUC
    Official _document getInitialProps() fn as provided by the styled components people.

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -3,13 +3,39 @@ import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   // Inject server side rendered styles into <head> for first render (and then load the rest in the client)
-  static getInitialProps({ renderPage }) {
+  // static getInitialProps({ renderPage }) {
+  //   const sheet = new ServerStyleSheet();
+  //   const page = renderPage((App) => (props) =>
+  //     sheet.collectStyles(<App {...props} />)
+  //   );
+  //   const styleTags = sheet.getStyleElement();
+  //   return { ...page, styleTags };
+  // }
+
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
-    const page = renderPage((App) => (props) =>
-      sheet.collectStyles(<App {...props} />)
-    );
-    const styleTags = sheet.getStyleElement();
-    return { ...page, styleTags };
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
