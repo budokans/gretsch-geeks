57dcc308d96825edfae95e02ea577482143ad4ac
budokans <68995729+budokans@users.noreply.github.com>
2021-05-19 15:54:33 +1200

    Update favicons and meta tags

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,7 +1,7 @@
-import Head from 'next/head';
-import PropTypes from 'prop-types';
-import styled, { createGlobalStyle } from 'styled-components';
-import Header from './Header';
+import Head from "next/head";
+import PropTypes from "prop-types";
+import styled, { createGlobalStyle } from "styled-components";
+import Header from "./Header";
 
 const GlobalStyles = createGlobalStyle`
   @font-face {
@@ -77,10 +77,27 @@ export default function Page({ children }) {
   return (
     <>
       <Head>
-        <link rel="shortcut icon" href="/static/favicon.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
         <meta
           name="description"
-          content="A very overpriced apparel buy-and-sell store."
+          content="A place to buy and sell your Gretsch drums."
         />
       </Head>
       <GlobalStyles />
